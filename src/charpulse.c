bd088b98c261bd8122e9bd1b6d439a5c3895a851
Sreeraj S Kurup <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2025-11-23 11:54:39 +0000

    Convert CharPulse buffer to dynamic, auto-resizing memory

--- src/charpulse.c
+++ src/charpulse.c
@@ -1,14 +1,12 @@
-// Developer: Sreeraj
-// GitHub: https://github.com/s-r-e-e-r-a-j
-
 #include "../include/charpulse.h"
 
 static dev_t cp_dev;
 static struct cdev cp_cdev;
 static struct class *cp_class;
 static struct device *cp_device;
-static char cp_buf[CHARPULSE_BUF];
-static size_t cp_len;
+static char *cp_buf;
+static size_t cp_len = 0;
+static size_t buffer_size = CHARPULSE_BUF;
 static DEFINE_MUTEX(cp_lock);
 
 static struct file_operations cp_ops = {
@@ -73,7 +71,7 @@ ssize_t cp_write(struct file *file, const char __user *in, size_t len, loff_t *o
             if (kbuf[len - 1] == '\r') kbuf[len - 1] = '\0';
 
             if (strcmp(kbuf, "clear") == 0) {
-                memset(cp_buf, 0, sizeof(cp_buf));
+                memset(cp_buf, 0, buffer_size);
                 cp_len = 0;
                 *off = 0;
                 mutex_unlock(&cp_lock);
@@ -93,13 +91,16 @@ ssize_t cp_write(struct file *file, const char __user *in, size_t len, loff_t *o
         goto out;
     }
 
-    if (pos >= CHARPULSE_BUF) {
-        ret = -ENOSPC;
-        goto out;
+    if (pos + len > buffer_size) {
+        size_t new_size = max(buffer_size * 2, pos + len);
+        char *new_buf = krealloc(cp_buf, new_size, GFP_KERNEL);
+        if (!new_buf) {
+            mutex_unlock(&cp_lock);
+            return -ENOMEM;
+        }
+        cp_buf = new_buf;
+        buffer_size = new_size;
     }
-
-    if (len > CHARPULSE_BUF - pos)
-        len = CHARPULSE_BUF - pos;
 
     if (copy_from_user(cp_buf + pos, in, len)) {
         ret = -EFAULT;
@@ -140,7 +141,7 @@ loff_t cp_llseek(struct file *file, loff_t off, int whence) {
             return -EINVAL;
     }
 
-    if (newpos < 0 || newpos > CHARPULSE_BUF) {
+    if (newpos < 0 || newpos > cp_len) {
         mutex_unlock(&cp_lock);
         return -EINVAL;
     }
@@ -186,7 +187,15 @@ int cp_init(void) {
     }
 
     mutex_init(&cp_lock);
-    memset(cp_buf, 0, sizeof(cp_buf));
+    cp_buf = kmalloc(buffer_size, GFP_KERNEL);
+    if (!cp_buf) {
+        device_destroy(cp_class, cp_dev);
+        class_destroy(cp_class);
+        cdev_del(&cp_cdev);
+        unregister_chrdev_region(cp_dev, 1);
+        return -ENOMEM;
+    }
+    memset(cp_buf, 0, buffer_size);
     cp_len = 0;
 
     printk(KERN_INFO "charpulse: module loaded successfully\n");
@@ -195,6 +204,7 @@ int cp_init(void) {
 
 void cp_exit(void) {
     printk(KERN_INFO "charpulse: unloading module...\n");
+    kfree(cp_buf);
     device_destroy(cp_class, cp_dev);
     class_destroy(cp_class);
     cdev_del(&cp_cdev);
@@ -207,4 +217,5 @@ module_exit(cp_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sreeraj S Kurup");
-MODULE_DESCRIPTION("CharPulse character device driver with read, write, append, and buffer clear support");
+MODULE_DESCRIPTION("CharPulse character device driver with read, write, append, clear, and dynamic buffer resizing support");
+
